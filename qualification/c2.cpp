649669204cd27b90fdd84215d35a489c2a9869f5
Hebi Li <lihebi.com@gmail.com>
2016-04-15 19:50:32 -0500

    init; qualification

---
+++ qualification/c2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int T;
+  cin>>T;
+  int N;
+  int J;
+  /**
+   * lenght of N, J output jamcoin
+   */
+  cin >> N >> J;
+
+  cout << "Case #1:\n";
+  solve(N, J);
+}
